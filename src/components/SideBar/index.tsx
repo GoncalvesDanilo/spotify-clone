f94f43ecc3ba080539c0ec4f48386ed05a44c820
Danilo <danilo.in.development@gmail.com>
2023-06-22 20:09:12 -0300

    💄 Update SideBar UI to new Spotify layout

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,11 @@
 import Icons from '../Icons';
+import { playlists } from './helper';
 import styles from './SideBar.module.css';
 
 const SideBar = () => {
   return (
     <div className={styles.sidebar}>
-      <Icons.SpotifyLogo className={styles.logo} />
-      <div className={styles.options}>
+      <div className={styles.section}>
         <div className={styles.option}>
           <Icons.HouseActive className={styles.icon} />
           <p>Home</p>
@@ -14,53 +14,64 @@ const SideBar = () => {
           <Icons.Search className={styles.icon} />
           <p>Search</p>
         </div>
-        <div className={styles.option}>
-          <Icons.Library className={styles.icon} />
-          <p>Your Library</p>
-        </div>
       </div>
-      <div className={styles.options}>
-        <div className={styles.option}>
-          <div className={styles.iconWrapper + ' ' + styles.plus}>
-            <Icons.Plus />
+      <div className={styles.section + ' ' + styles.library}>
+        <div className={styles.header}>
+          <div className={styles.option}>
+            <Icons.LibraryActive className={styles.icon} />
+            <p>Your Library</p>
           </div>
-          <p>Create Playlist</p>
+          <div className={styles.actions}>
+            <div className={styles.action}>
+              <Icons.Plus className={styles.icon} />
+            </div>
+            <div className={styles.action}>
+              <Icons.Arrow className={styles.icon} />
+            </div>
+          </div>
         </div>
-        <div className={styles.option}>
-          <div className={styles.iconWrapper + ' ' + styles.heart}>
-            <Icons.HeartActive />
+        <div className={styles.filters}>
+          <div className={styles.container}>
+            <div className={styles.filter}>
+              <p>Playlists</p>
+            </div>
+            <div className={styles.filter}>
+              <p>Artists</p>
+            </div>
+            <div className={styles.filter}>
+              <p>Albums</p>
+            </div>
+            <div className={styles.filter}>
+              <p>Podcasts & Shows</p>
+            </div>
           </div>
-          <p>Liked Songs</p>
+          <div className={styles.searchContainer}>
+            <div className={styles.search}>
+              <div className={styles.iconWrapper}>
+                <Icons.Search className={styles.icon} />
+              </div>
+            </div>
+            <div className={styles.sort}>
+              <p>Recents</p>
+              <Icons.DropdownArrow className={styles.icon} />
+            </div>
+          </div>
         </div>
-      </div>
-      <hr className={styles.separator} />
-      <div className={styles.playlists}>
-        {[
-          'Playlist 1',
-          'Playlist 2',
-          'Playlist 3',
-          'Playlist 4',
-          'Playlist 5',
-          'Playlist 1',
-          'Playlist 2',
-          'Playlist 3',
-          'Playlist 4',
-          'Playlist 5',
-          'Playlist 1',
-          'Playlist 2',
-          'Playlist 3',
-          'Playlist 4',
-          'Playlist 5',
-          'Playlist 1',
-          'Playlist 2',
-          'Playlist 3',
-          'Playlist 4',
-          'Playlist 5',
-        ].map((playlist) => (
-          <div className={styles.playlist}>
-            <p>{playlist}</p>
-          </div>
-        ))}
+        <div className={styles.playlists}>
+          {playlists.map((playlist) => (
+            <div className={styles.playlist}>
+              <div className={styles.image}>
+                <img src={playlist.image} alt="playlist cover" />
+              </div>
+              <div className={styles.info}>
+                <div className={styles.name}>{playlist.name}</div>
+                <div className={styles.details}>
+                  {playlist.type} • {playlist.detail}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
