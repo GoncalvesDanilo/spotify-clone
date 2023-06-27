09e85c352db490cc21d363e7542a9185d41573fe
Danilo <danilo.in.development@gmail.com>
2023-06-27 16:57:14 -0300

    ✨ Add useComponentVisible hook

---
+++ src/hooks/useComponentVisible.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useComponentVisible = (initialIsVisible: boolean) => {
+  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+};
+
+export default useComponentVisible;
