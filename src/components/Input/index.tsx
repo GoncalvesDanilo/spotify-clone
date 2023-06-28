690c12150ebccecae553fb06ff9fb91e1f804fa2
Danilo <danilo.in.development@gmail.com>
2023-06-28 15:23:15 -0300

    ✨ Add Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import Icons from '../Icons';
 import styles from './Input.module.css';
 
-const Input = () => {
+type InputProps = {
+  type: string;
+  placeholder?: string;
+  onChange?: (value: string) => void;
+  defaultValue?: string;
+};
+
+const Input = ({ type, placeholder, onChange, defaultValue }: InputProps) => {
+  const [value, setValue] = useState(defaultValue || '');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setValue(value);
+    if (onChange) onChange(value);
+  };
+
+  const handleClearClick = () => {
+    setValue('');
+    if (onChange) onChange('');
+  };
+
   return (
-    <div className={styles.container}>
-      <Icons.Search className={styles.icon} />
-      <input type="text" placeholder="What do you want to listen to?" />
+    <div className={styles.container + (isFocused ? ' ' + styles.focused : '')}>
+      <Icons.Search className={styles.icon + ' ' + styles.search} />
+      <input
+        type={type}
+        placeholder={placeholder || ''}
+        value={value}
+        onChange={handleChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      {value && (
+        <Icons.Cross className={styles.icon + ' ' + styles.clear} onClick={handleClearClick} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Input;
